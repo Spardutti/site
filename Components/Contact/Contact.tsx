992f697562c517854101b8245158305f162e143e
spardutti <luisdamian.sp@gmail.com>
2022-01-20 18:47:30 -0300

    impreved contact me buttons, added hvoer

--- Components/Contact/Contact.tsx
+++ Components/Contact/Contact.tsx
@@ -1,93 +1,56 @@
-import { Box, Heading, Link, Stack, Text } from "@chakra-ui/react";
+import { Box, Heading, Link, Stack } from "@chakra-ui/react";
 import { ImWhatsapp } from "react-icons/im";
 import { SiGmail } from "react-icons/si";
+import { BsGithub } from "react-icons/bs";
+import { BsLinkedin } from "react-icons/bs";
+import { motion } from "framer-motion";
 
-type Info = {
-  url: string;
-  icon: string;
-}[];
-
-const contactInfo: Info = [
-  {
-    url: "https://www.linkedin.com/in/spardutti/",
-    icon: "devicon-linkedin-plain colored",
-  },
-  {
-    url: "https://github.com/Spardutti",
-    icon: "devicon-github-original-wordmark",
-  },
-];
+const MotionLink = motion(Link);
 
 export const Contact: React.FC<{ language: string }> = ({ language }) => {
-  const Es = () => {
-    return (
-      <Box py={10} textAlign={"center"}>
-        <Heading fontSize={[50, 60, 80]}>Contactame</Heading>
-        <Stack pt={10} justifyContent={"center"} direction={"row"}>
-          {contactInfo.map((elem, index) => {
-            return (
-              <Link href={elem.url} key={index} target={"_blank"}>
-                <Text fontSize={60} px={[1, 10]} className={elem.icon}></Text>
-              </Link>
-            );
-          })}
-          <Link
-            pr={[1, 10]}
-            href={"mailto:luisdamian.sp@gmail.com?Subject=Hola " + "Damian"}
-            target="_blank"
-            textColor={"red.700"}
-          >
-            <SiGmail fontSize={60} />
-          </Link>
-          <Link
-            textColor={"whatsapp.600"}
-            href="https://wa.me/+5491138629691"
-            target="_blank"
-          >
-            <ImWhatsapp fontSize={60} />
-          </Link>
-        </Stack>
-      </Box>
-    );
-  };
-
-  const En = () => {
-    return (
-      <Box py={10} textAlign={"center"}>
-        <Heading fontSize={[50, 60, 80]}>Contact Me</Heading>
-        <Stack pt={10} justifyContent={"center"} direction={"row"}>
-          {contactInfo.map((elem, index) => {
-            return (
-              <Link href={elem.url} key={index} target={"_blank"}>
-                <Text
-                  fontSize={[40, 40, 60]}
-                  px={[1, 10]}
-                  className={elem.icon}
-                ></Text>
-              </Link>
-            );
-          })}
-          <Link
-            px={[1, 10]}
-            href={"mailto:luisdamian.sp@gmail.com?Subject=Hello " + "Damian"}
-            target="_blank"
-            textColor={"red.700"}
-            fontSize={[40, 40, 60]}
-          >
-            <SiGmail />
-          </Link>
-          <Link
-            textColor={"whatsapp.600"}
-            href="https://wa.me/+5491138629691"
-            target="_blank"
-            fontSize={[40, 40, 60]}
-          >
-            <ImWhatsapp />
-          </Link>
-        </Stack>
-      </Box>
-    );
-  };
-
-  return <div id="contact">{language === "es" ? <Es /> : <En />}</div>;
+  return (
+    <Box py={10} textAlign={"center"} id="contact">
+      <Heading fontSize={[50, 60, 80]}>
+        {language === "es" ? "Contactame" : "Contact Me"}
+      </Heading>
+      <Stack
+        pt={10}
+        justifyContent={"space-around"}
+        direction={"row"}
+        fontSize={60}
+      >
+        <MotionLink
+          href="https://www.linkedin.com/in/spardutti/"
+          target={"_blank"}
+          textColor={"#458eff"}
+          whileHover={{ scale: 1.2 }}
+        >
+          <BsLinkedin />
+        </MotionLink>
+        <MotionLink
+          whileHover={{ scale: 1.2 }}
+          href="https://github.com/Spardutti"
+          target={"_blank"}
+        >
+          <BsGithub />
+        </MotionLink>
+        <MotionLink
+          href={"mailto:luisdamian.sp@gmail.com?Subject=Hola " + "Damian"}
+          target="_blank"
+          whileHover={{ scale: 1.2 }}
+          textColor={"#BB001B"}
+        >
+          <SiGmail />
+        </MotionLink>
+        <MotionLink
+          textColor={"whatsapp.600"}
+          href="https://wa.me/+5491138629691"
+          whileHover={{ scale: 1.2 }}
+          target="_blank"
+        >
+          <ImWhatsapp />
+        </MotionLink>
+      </Stack>
+    </Box>
+  );
 };
